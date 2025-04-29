877acb3bed5941c8046e718484f70e2a92c83a24
Aran Kim <95355746+devarankim@users.noreply.github.com>
2025-04-29 23:23:42 +0900

    20250429 오늘 공부분 커밋

---
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE) //타겟이 중요함. TYPE는 클래스 레벨에 붙는거
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
